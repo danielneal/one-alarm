028842a3b0d070799823808097fd2e3f3c757cf3
Daniel Neal <danielneal@Daniels-MacBook-Air.local>
2020-11-23 13:15:25 +0000

    Functional Countdown

--- components/AlarmCountdown.js
+++ components/AlarmCountdown.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { StyleSheet, Text, View } from 'react-native';
 import intervalToDuration from 'date-fns/intervalToDuration'
 import formatDuration from 'date-fns/formatDuration'
 import isAfter from 'date-fns/isAfter'
 
 export default function AlarmCountdown(props) {
+  let [now, setNow] = useState(new Date())
   let duration = formatDuration(intervalToDuration({start:new Date(),end:props.date}))
+  useEffect(() => {
+    let intervalId = setInterval(() => { setNow(new Date())}, 1000)
+    return ()=> {
+      clearInterval(intervalId)
+    }},
+  [])
+
   return (
     <View>
       {isAfter(props.date, new Date()) ?
