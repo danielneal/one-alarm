5fa2c0b49351335df8742d1d78e03a7851930071
Daniel Neal <danielneal@Daniels-MacBook-Air.local>
2020-11-25 18:13:25 +0000

    Parse text input

--- App.tsx
+++ App.tsx
@@ -1,17 +1,32 @@
 import { StatusBar } from "expo-status-bar";
-import React, { useCallback, useState, useEffect } from "react";
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import React, { useCallback, useState, useEffect, useRef } from "react";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  InputAccessoryView,
+  Button,
+  Keyboard,
+} from "react-native";
 import * as Svg from "react-native-svg";
 import Clock from "./components/Clock";
 import ClockDigital from "./components/ClockDigital";
 import AlarmCountdown from "./components/AlarmCountdown";
-import { State, PanGestureHandler } from "react-native-gesture-handler";
+import {
+  State,
+  PanGestureHandler,
+  TapGestureHandler,
+} from "react-native-gesture-handler";
 import * as Notifications from "expo-notifications";
 import roundToNearestMinutes from "date-fns/roundToNearestMinutes";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import add from "date-fns/add";
 import isAfter from "date-fns/isAfter";
+import isValid from "date-fns/isValid";
 import parseISO from "date-fns/parseISO";
+import parse from "date-fns/parse";
 import formatISO from "date-fns/formatISO";
 import useForceUpdate from "./hooks/useForceUpdate";
 const alarmStorageKey = "@alarm";
@@ -58,13 +73,22 @@ export default function App() {
   const [alarmState, setAlarmState] = useState({ time: null });
   const [handlerState, setHandlerState] = useState();
   const [alarmTimeAtGestureBegin, setAlarmTimeAtGestureBegin] = useState(null);
+  const [timeText, setTimeText] = useState();
   const forceUpdate = useForceUpdate();
+  const textInput = useRef();
+  const panGestureHandler = useRef();
 
   const saveAlarmState = async () => {
     if (alarmState !== null) {
       AsyncStorage.setItem(alarmStorageKey, serialize(alarmState));
     }
   };
+
+  const onTapHandlerStateChange = useCallback((e) => {
+    if (e.nativeEvent.state === State.ACTIVE) {
+      textInput.current.focus();
+    }
+  });
 
   const onHandlerStateChange = useCallback(async (e) => {
     // capture the alarm time at the beginning of the gesture
@@ -132,24 +156,68 @@ export default function App() {
     };
   }, [alarmState]);
 
+  const setAlarmFromText = () => {
+    let parsedTime;
+    console.log(timeText);
+    switch (timeText.length) {
+      case 1:
+        parsedTime = parse(timeText, "H", new Date());
+        console.log(parsedTime);
+        break;
+      case 2:
+        parsedTime = parse(timeText, "HH", new Date());
+        break;
+      case 3:
+        parsedTime = parse(timeText, "Hmm", new Date());
+        break;
+      case 4:
+        parsedTime = parse(timeText, "HHmm", new Date());
+        break;
+    }
+    if (isValid(parsedTime)) {
+      setAlarmState({
+        time: isAfter(parsedTime, new Date())
+          ? parsedTime
+          : add(parsedTime, { days: 1 }),
+      });
+    }
+    Keyboard.dismiss();
+  };
+
   let renderTime = alarmState.time === null ? new Date() : alarmState.time;
   // if alarm time is null, render the current time
   return (
     <SafeAreaView>
-      <PanGestureHandler
-        onHandlerStateChange={onHandlerStateChange}
-        onGestureEvent={onGestureEvent}
+      <TapGestureHandler
+        onHandlerStateChange={onTapHandlerStateChange}
+        waitFor={panGestureHandler}
       >
-        <View style={styles.container}>
-          {renderTime && (
-            <>
-              <Clock date={renderTime} />
-              <ClockDigital date={renderTime} />
-              <AlarmCountdown date={renderTime} />
-            </>
-          )}
-        </View>
-      </PanGestureHandler>
+        <PanGestureHandler
+          onHandlerStateChange={onHandlerStateChange}
+          onGestureEvent={onGestureEvent}
+          ref={panGestureHandler}
+        >
+          <View style={styles.container}>
+            {renderTime && (
+              <>
+                <Clock date={renderTime} />
+                <ClockDigital date={renderTime} />
+                <AlarmCountdown date={renderTime} />
+                <TextInput
+                  ref={textInput}
+                  style={styles.textInput}
+                  keyboardType="number-pad"
+                  inputAccessoryViewID="inputAccessory"
+                  onChangeText={(s) => setTimeText(s)}
+                />
+                <InputAccessoryView nativeID="inputAccessory">
+                  <Button title="Done" onPress={setAlarmFromText} />
+                </InputAccessoryView>
+              </>
+            )}
+          </View>
+        </PanGestureHandler>
+      </TapGestureHandler>
     </SafeAreaView>
   );
 }
@@ -159,5 +227,10 @@ const styles = StyleSheet.create({
     height: "100%",
     alignItems: "center",
     justifyContent: "flex-start",
+  },
+  textInput: {
+    width: "100%",
+    backgroundColor: "red",
+    height: 20,
   },
 });
--- hooks/useForceUpdate.tsx
+++ hooks/useForceUpdate.tsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 
 export default function useForceUpdate() {
-  const [state, setState] = useState(new Date());
+  const [, setState] = useState();
   const forceUpdate = () => {
-    setState(new Date());
+    setState();
   };
   return forceUpdate;
 }
